6ad4e94e8b3e0e410a30c003caaf107b4362736f
LUCAS PRIMATI MENEZES <16.00683-6@maua.br>
2019-03-18 11:11:28 -0300

    Projeto criado com netbeans

---
+++ pratica2/Nova Pasta/meuPrimeiroProjetoJava/src/meuprimeiroprojetojava/MeuPrimeiroProjetoJava.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package meuprimeiroprojetojava;
+
+/**
+ *
+ * @author 16.00683-6
+ */
+public class MeuPrimeiroProjetoJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Ola Mundo");
+    }
+    
+}
